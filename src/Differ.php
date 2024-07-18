adc5f05e1e59fc5525a5d6c29fff9e6eba1dd099
Ivan Slovinsky <slovanya1@gmail.com>
2024-07-18 21:57:50 +0300

    Fix

--- src/Differ.php
+++ src/Differ.php
@@ -19,24 +19,23 @@ function makeDiff(array $before, array $after): array
     $keys = array_unique(array_merge(array_keys($before), array_keys($after)));
     $unionKeys = sort($keys, fn($left, $right) => strcmp($left, $right));
     return array_map(function ($key) use ($before, $after) {
-        $node = [];
         if (array_key_exists($key, $before) && array_key_exists($key, $after)) {
             if (is_array($before[$key]) && is_array($after[$key])) {
-                $node = buildNode('nested', $key, null, null, makeDiff($before[$key], $after[$key]));
+                return buildNode('nested', $key, null, null, makeDiff($before[$key], $after[$key]));
             } elseif ($before[$key] === $after[$key]) {
-                $node = buildNode("unchanged", $key, $before[$key], $after[$key]);
+                return buildNode("unchanged", $key, $before[$key], $after[$key]);
             } else {
-                $node = buildNode("changed", $key, $before[$key], $after[$key]);
+                return buildNode("changed", $key, $before[$key], $after[$key]);
             }
         }
         if (array_key_exists($key, $before) && !array_key_exists($key, $after)) {
-            $node = buildNode("removed", $key, $before[$key], null);
+            return buildNode("removed", $key, $before[$key], null);
         }
         if (!array_key_exists($key, $before) && array_key_exists($key, $after)) {
-            $node = buildNode("added", $key, null, $after[$key]);
+            return buildNode("added", $key, null, $after[$key]);
         }
 
-        return $node;
+        return [];
     }, $unionKeys);
 }
 
